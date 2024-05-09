61d9b2a7093f2eacffd518e1f1f5bd1695d0b5a5
Evelyn Resendiz Balderas <chevelynda@hotmail.com>
2024-05-09 00:25:33 -0400

    server set up

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,5 +1,21 @@
 const SignUp = () => {
-  return <p>Sign Up</p>;
+  return (
+    <div>
+      <h1>Registration</h1>
+      <form>
+        <label for="name">Name:</label>
+        <input type="text" required />
+
+        <label for="email">Email:</label>
+        <input type="email" required />
+
+        <label for="password">Password:</label>
+        <input type="password" required />
+
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
